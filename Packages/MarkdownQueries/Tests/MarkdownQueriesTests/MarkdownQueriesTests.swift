a2594432aff1799321f8ecaaf88bb61134f73101
ninuo.dong <ninuo.dong@tuya.com>
2022-07-03 13:49:44 +0800

    faet: 调整 Package 依赖方式

--- Packages/MarkdownQueries/Tests/MarkdownQueriesTests/MarkdownQueriesTests.swift
+++ Packages/MarkdownQueries/Tests/MarkdownQueriesTests/MarkdownQueriesTests.swift
@@ -6,6 +6,5 @@ final class MarkdownQueriesTests: XCTestCase {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct
         // results.
-        XCTAssertEqual(MarkdownQueries().text, "Hello, World!")
     }
 }
